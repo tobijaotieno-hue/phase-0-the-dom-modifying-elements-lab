36b74e286499cdb7d3bc872036c949530b998e62
tobijaotieno-hue <tobijaotieno@gmail.com>
2025-10-01 07:50:23 +0300

    completed dom modifying element lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainTag = document.querySelector("#main")
+
+mainTag.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "Alex is the champion"
